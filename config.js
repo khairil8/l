b358b47cc532c2731b2e4307547d84d35e6a1491
khairil <80386217+khairil8@users.noreply.github.com>
2023-01-15 21:57:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -8,16 +8,16 @@ import axios from 'axios'
 import moment from 'moment-timezone'
 
 global.owner = [
-  ['923470027813', '👑 Ahmad Ali - Creador 👑', true],
-  ['19294515952']
+  ['60109102984', '👑 Khairil春 👑', true],
+  ['601124024797']
 ] 
-global.suittag = ['923470027813'] 
-global.prems = ['923470027813','19294515952'] 
+global.suittag = ['60109102984'] 
+global.prems = ['60109102984','601124024797'] 
 
 global.packname = 'x_Ikratos_x'
-global.author = '⍟Ahmad » Ali⍟'
-global.wm = '⍟Ahmad » Ali⍟ V5.3'
-global.igfg = '⍟Ahmad » Ali⍟'
+global.author = '⍟Khairil春⍟'
+global.wm = '⍟Luffy愛⍟ V5.3'
+global.igfg = '⍟Luffy愛⍟'
 global.wait = '*[❗] Wait, Processing...*'
 
 global.keysZens = ['fiktod', 'c2459db922', 'BF39D349845E', '675e34de8a', '37CC845916', '0b917b905e6f', '6fb0eff124']
@@ -437,14 +437,14 @@ global.flaaa = [
 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=']
-global.gt = '⍟Ahmad » Ali⍟'
-global.mysticbot = '⍟Ahmad » Ali⍟'
-global.md = 'https://github.com/xIKRATOSx/Shizu-Bot-MD'
-global.mysticbot = 'https://github.com/xIKRATOSx/Shizu-Bot-MD'
+global.gt = '⍟Khairil春⍟'
+global.mysticbot = '⍟Khairil春⍟'
+global.md = 'https://github.com/KhairilOFC'
+global.mysticbot = 'https://linkfly.to/Khairil'
 global.waitt = '*[❗] Processing, Wait a moment...*'
 global.waittt = '*[❗] Processing, Wait a moment...*'
 global.waitttt = '*[❗] Processing, Wait a moment...*'
-global.nomorown = '923470027813'
+global.nomorown = '60109102984'
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf']
 global.cmenut = '❖––––––『'
 global.cmenub = '┊✦ '
